841bb9f6e9d1379822703ce1ff937e482dae8318
= <jmiera@byu.edu>
2020-09-22 13:01:51 -0600

    Modified to allow filename customization

--- snap.py
+++ snap.py
@@ -5,7 +5,7 @@ from picamera import PiCamera
 from time import sleep
 
 
-def main(num_minutes):
+def main(num_minutes, fname):
     print("Trace 5")
     camera = PiCamera()
     print("Trace 6")
@@ -13,7 +13,7 @@ def main(num_minutes):
     print("Trace 7")
     for i in range(num_minutes):
         sleep(60)
-        camera.capture("snapshots/snapshot%s.jpg" % i)
+        camera.capture("snapshots/" + fname + "%s.jpg" % i)
         print("Trace 8")
         print("Saved picture in snapshots/snapshot%s.jpg" % i)
     camera.stop_preview()
@@ -23,9 +23,12 @@ def main(num_minutes):
 if __name__ == "__main__":
     print("Trace 1")
     num_minutes = 10
+    fname = "snapshot"
     if len(sys.argv) > 1:
         print("Trace 2")
         num_minutes = int(sys.argv[1])
+    if len(sys.argv) > 2:
+        fname = sys.argv[2]
     print("Trace 3")
-    main(num_minutes)
+    main(num_minutes, fname)
     print("Trace 4")
