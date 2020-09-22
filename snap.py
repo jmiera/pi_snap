3fe230e4c0f4b2c29d2de0f19eda37e9b10bac81
= <jmiera@byu.edu>
2020-09-22 13:08:59 -0600

    Removed trace statements, fixed print statement, added new print statements

--- snap.py
+++ snap.py
@@ -6,29 +6,22 @@ from time import sleep
 
 
 def main(num_minutes, fname):
-    print("Trace 5")
     camera = PiCamera()
-    print("Trace 6")
     camera.start_preview()
-    print("Trace 7")
     for i in range(num_minutes):
         sleep(60)
         camera.capture("snapshots/" + fname + "%s.jpg" % i)
-        print("Trace 8")
-        print("Saved picture in snapshots/snapshot%s.jpg" % i)
+        print("Saved picture in snapshots/" + fname + "%s.jpg" % i)
     camera.stop_preview()
-    print("Trace 9")
 
 
 if __name__ == "__main__":
-    print("Trace 1")
     num_minutes = 10
     fname = "snapshot"
     if len(sys.argv) > 1:
-        print("Trace 2")
         num_minutes = int(sys.argv[1])
     if len(sys.argv) > 2:
         fname = sys.argv[2]
-    print("Trace 3")
+    print("Running pi-snap for %i minutes" % num_minutes)
+    print("All files will be saved as " + fname + "#.jpg")
     main(num_minutes, fname)
-    print("Trace 4")
